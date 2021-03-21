f93c6c07d58fef3f1dd79250d674b5dc731dd3ec
Ratherman <ratherman5796@gmail.com>
2021-03-21 12:13:24 +0800

    XGBoost

--- DeepLearning/HW1/XGBoost.py
+++ DeepLearning/HW1/XGBoost.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-root_path = "C:/Users/USER/Desktop/Projects/Github_Repo/AI/DeepLearning/"
+root_path = "C:/Users/AC/Desktop/Github/AI/DeepLearning/HW1/"
 train_X = pd.read_csv(root_path + "Train_COH_Dataset_all_256.csv", header=None)
 train_Y = pd.read_csv(root_path + "Train_COH_Label_all_256.csv", header=None)
 train_Y = train_Y[0].values
@@ -17,18 +17,34 @@ test_Y = test_Y[0].values
 test_Y = test_Y.astype(int)
 
 import time
-from xgboost import XGBClassifier
+import xgboost as xgb
 
-xgbc_model = XGBClassifier()
+dtrain = xgb.DMatrix(train_X, label=train_Y)
+dval = xgb.DMatrix(val_X, label=val_Y)
+dtest = xgb.DMatrix(test_X, label=test_Y)
 
+param = {
+    "max_depth":3, # the maximum number of teach tree
+    "eta":0.01, # the training step for each iteration
+    "silent":1, # loggin mode - quiet
+    "objective": "multi:softprob", # error evaluation for multiclass training
+    "num_class":50 # the number of classes exists in this datasets
+}
+
+num_round = 30
 tic = time.time()
-xgbc_model.fit(train_X, train_Y)
+bst = xgb.train(param, dtrain, num_round)
 toc = time.time()
-print(f"[Analysis] I have spent {round(toc - tic, 2)} sec to train the RF model.")
 
-pred_val = xgbc_model.predict(val_X)
-pred_test = xgbc_model.predict(test_X)
+print(f"[Analysis] I have spent {round(toc - tic, 2)} sec to train the XGboost model.")
+
+preds_val = bst.predict(dval)
+preds_test = bst.predict(dtest)
+
+import numpy as np
+best_pred_val = np.asarray([np.argmax(line) for line in preds_val])
+best_pred_test = np.asarray([np.argmax(line) for line in preds_test])
 
 from sklearn import metrics
-print(f"[Result of Val] Accuracy = {metrics.accuracy_score(val_Y, pred_val)}.")
-print(f"[Result of Test] Accuracy = {metrics.accuracy_score(test_Y, pred_test)}.")
+print(f"[Result of Val] Accuracy = {round(metrics.accuracy_score(val_Y, best_pred_val) * 100, 2)} %.")
+print(f"[Result of Test] Accuracy = {round(metrics.accuracy_score(test_Y, best_pred_test) * 100, 2)} %.")
